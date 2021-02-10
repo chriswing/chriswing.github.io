383c84bb8d49a59cb55ee6bc31d2660ad8ab2df5
Chris WIng <chriswing@wing2804@gmail.com>
2021-02-10 19:12:52 +0100

    project

---
+++ m133/Project/js/calendar.js
@@ -0,0 +1,103 @@
+//Full Calendar Settings und Event eintragen
+const scheduleUrl = "https://sandbox.gibm.ch/tafel.php";
+var calendarEl = document.getElementById("calendar");
+var events = [];
+
+//Plugins for Callendar
+var calendar = new FullCalendar.Calendar(calendarEl, {
+  plugins: ["timeGrid", "bootstrap"],
+  defaultView: "timeGridWeek",
+  handleWindowResize: true,
+  height: "auto",
+  weekends: false,
+  allDaySlot: false,
+  nowIndicator: true,
+  locale: "de",
+  timeZone: "local",
+  minTime: "07:00:00",
+  maxTime: "20:00:00",
+  themeSystem: "bootstrap",
+  footer: false,
+  header: {
+    left: "prevDate, nextDate",
+    center: "title",
+    right: "openModal,todayB",
+  },
+  //custom buttons for the calendar
+  customButtons: {
+    openModal: {
+      text: "settings",
+      bootstrapFontAwesome: "fas fa-cog",
+      click: function () {
+        $("#myModal").modal("show");
+      },
+    },
+    nextDate: {
+      text: ">",
+      bootstrapFontAwesome: "fas fa-step-forward",
+      click: function () {
+        calendar.next();
+        initializeCalendar();
+      },
+    },
+    prevDate: {
+      text: "<",
+      bootstrapFontAwesome: "fas fa-step-backward",
+      click: function () {
+        calendar.prev();
+        initializeCalendar();
+      },
+    },
+    todayB: {
+      text: "today",
+      bootstrapFontAwesome: "fas fa-calendar-day",
+      click: function () {
+        calendar.today();
+        initializeCalendar();
+      },
+    },
+  },
+});
+
+//initializes the calendar on load
+window.onload = function () {
+  $(".fc-toolbar.fc-header-toolbar").addClass("row col-lg-12");
+  initializeCalendar();
+};
+
+
+//creats the events in the calendar
+function initializeCalendar() {
+  events = [];
+  var now = moment(calendar.getDate());
+  var week = now.format('WW-YYYY');
+  console.log(week);
+  const url = `${scheduleUrl}?klasse_id=${localStorage.getItem(
+    "class"
+  )}&woche=${week}`;
+  $.getJSON(url, function (data) {
+    for (var table of data) {
+      var event = {
+        title: table.tafel_longfach,
+        start: table.tafel_datum + "T" + table.tafel_von,
+        end: table.tafel_datum + "T" + table.tafel_bis,
+        prof: table.tafel_lehrer,
+        room: table.tafel_raum,
+        comment: table.tafel_kommentar,
+        allDay: false,
+      };
+
+      events.push(event);
+    }
+  }).done(function () {
+    calendar.getEvents().forEach(removeEvents);
+    function removeEvents(eventObject) {
+      eventObject.remove();
+    }
+    calendar.getEvents().forEach((event) => event.remove());
+    events.forEach((event) => calendar.addEvent(event));
+  });
+
+  //renders the calendar with the events
+  calendar.render();
+}
---
+++ m133/Project/js/sort.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('class') === null && localStorage.getItem('job') === null) {
+      $('#myModal').modal('show');
+    }
+  
+    $('#berufe').on('change', function () {
+      getClass($('#berufe option:selected').val());
+    });
+  });
+  
+  $('#klassen').hide();
+  
+  $('#berufe').on('change', function () {
+    retriveClass();
+    setLocalStorage();
+  
+    $('#klassen').show();
+    setLocalStorage();
+  });
+  
+  function retriveClass() {
+    getClass($('#berufe option:selected').val());
+  }
+  
