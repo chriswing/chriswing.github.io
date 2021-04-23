9edcb9c11d0f6c1639b5f9d7f8ae41364680c080
Chris WIng <chriswing2804@gmail.com>
2021-04-23 14:41:02 +0200

    fill content

---
+++ portportfolio/pages/about.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+    <Head>
+      <title>Chris Wing | About</title>
+      <meta name="keywords" content="Chris Wing"/>
+    </Head>
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed
+        eleifend leo, sit amet ultrices eros. Donec at ipsum eget purus aliquam
+        sagittis. Duis id lectus sed ligula interdum ultrices.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed
+        eleifend leo, sit amet ultrices eros. Donec at ipsum eget purus aliquam
+        sagittis. Duis id lectus sed ligula interdum ultrices.
+      </p>
+    </div>
+    </>
+  );
+};
+
+export default About;
